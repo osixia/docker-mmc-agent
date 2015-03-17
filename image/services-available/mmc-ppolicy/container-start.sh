a262baaa3207fe4fc7e6dba1fb3377119e2892b9
Bertrand Gouny <bgouny@hi-media.com>
2015-03-17 18:05:23 +0100

    init

--- image/services-available/mmc-ppolicy/container-start.sh
+++ image/services-available/mmc-ppolicy/container-start.sh
@@ -1,6 +1,16 @@
 #!/bin/bash -e
 
+include /path/to/openldap/schema/ppolicy.schema
 
+# Load the ppolicy module
+moduleload  ppolicy
+
+# Add the overlay ppolicy to your OpenLDAP database
+database  bdb
+suffix    "dc=mandriva,dc=com"
+
+overlay ppolicy
+ppolicy_default "cn=default,ou=Password Policies,dc=mandriva,dc=com"
 
 
 
