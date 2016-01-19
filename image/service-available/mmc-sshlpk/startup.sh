c904b53c2eaadd394278d3aeed8d76c86a7cdd64
Bertrand Gouny <bgouny@hipay.com>
2016-01-19 13:44:31 +0100

    make image --copy-service friendly

--- image/service-available/mmc-sshlpk/startup.sh
+++ image/service-available/mmc-sshlpk/startup.sh
@@ -6,17 +6,16 @@ log-helper level eq trace && set -x
 
 
 FIRST_START_DONE="${CONTAINER_STATE_DIR}/docker-mmc-agent-sshlpk-first-start-done"
-
 # container first start
 if [ ! -e "$FIRST_START_DONE" ]; then
 
    # sshlpk plugin configuration
-   ${CONTAINER_STATE_DIR}/mmc-agent/assets/config-plugin.sh "$MMC_AGENT_SSHLPK_PLUGIN_CONFIG" /etc/mmc/plugins/sshlpk.ini
-   cp -f /etc/mmc/plugins/sshlpk.ini ${CONTAINER_STATE_DIR}/mmc-agent/assets/sshlpk.ini
+   ${CONTAINER_SERVICE_DIR}/mmc-agent/assets/config-plugin.sh "$MMC_AGENT_SSHLPK_PLUGIN_CONFIG" /etc/mmc/plugins/sshlpk.ini
+   cp -f /etc/mmc/plugins/sshlpk.ini ${CONTAINER_SERVICE_DIR}/mmc-agent/assets/sshlpk.ini
 
   touch $FIRST_START_DONE
 fi
 
-ln -sf ${CONTAINER_STATE_DIR}/mmc-agent/assets/sshlpk.ini /etc/mmc/plugins/sshlpk.ini
+ln -sf ${CONTAINER_SERVICE_DIR}/mmc-agent/assets/sshlpk.ini /etc/mmc/plugins/sshlpk.ini
 
 exit 0
