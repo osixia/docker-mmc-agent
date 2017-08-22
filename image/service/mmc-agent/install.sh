0f5b5aa289efa1e89bec95a47e491ec19687ce06
Bertrand Gouny <bertrand.gouny@osixia.net>
2017-08-22 16:49:48 +0200

    update packages

--- image/service/mmc-agent/install.sh
+++ image/service/mmc-agent/install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash -e
 # this script is run during the image build
 
-dpkg -i /container/service/mmc-agent/assets/package/python-mmc-core_3.9.92-1_all.deb
-dpkg -i /container/service/mmc-agent/assets/package/python-mmc-base_3.9.92-1_all.deb
-dpkg -i /container/service/mmc-agent/assets/package/python-mmc-dashboard_3.9.92-1_all.deb
-dpkg -i /container/service/mmc-agent/assets/package/mmc-agent_3.9.92-1_all.deb
+dpkg -i /container/service/mmc-agent/assets/package/python-mmc-core_3.9.90-10_all.deb
+dpkg -i /container/service/mmc-agent/assets/package/python-mmc-base_3.9.90-10_all.deb
+dpkg -i /container/service/mmc-agent/assets/package/python-mmc-dashboard_3.9.90-10_all.deb
+dpkg -i /container/service/mmc-agent/assets/package/mmc-agent_3.9.90-10_all.deb
 
 rm -rf /container/service/mmc-agent/assets/package
 
 # mmc-agent config
 sed -i -e "s/#*\s*host\s*=.*/host = 0.0.0.0/" /etc/mmc/agent/config.ini
+sed -i -e "s|#*\s*userHomeAction\s*=.*|userHomeAction = 0|" /etc/mmc/plugins/base.ini
 
 mkdir -p /home/archives /var/log/mmc/
